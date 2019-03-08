d58d441f34040c793ad5831b4db09f3c6c00910c
2448963982@qq.com <2448963982@qq.com>
2019-03-08 10:49:03 +0800

    首次提交初始化项目

---
+++ src/main/java/com/zxb/iceandsnow/IceandsnowApplication.java
@@ -0,0 +1,13 @@
+package com.zxb.iceandsnow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IceandsnowApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(IceandsnowApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zxb/iceandsnow/IceandsnowApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zxb.iceandsnow;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class IceandsnowApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
